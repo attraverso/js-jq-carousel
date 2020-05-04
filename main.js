c8d38115ed34df535955cc89c302e231fb080e40
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-05-04 15:17:34 +0200

    timed slider complete

--- main.js
+++ main.js
@@ -1,36 +1,50 @@
 //creo una variabile con cu iidentificare l'immagine corrente attraverso la classe current-img
 
 //intercetto il click
-$('.next-box').click(function() {
-  var currentImg = $('img.current-img');
-
+setInterval(function() {
+  var currentImg = $('img.current');
+  var currentDot = $('.bullets-box i.current')
   //rimuovo la classe
-  currentImg.removeClass('current-img');
-
+  currentImg.removeClass('current');
+  currentDot.removeClass('fas current');
+  currentDot.addClass('far');
   var nextImg = currentImg.next();
-  //scalo di uno e aggiungo la classe
+  var nextDot = currentDot.next();
 
+  //scalo di uno e aggiungo la classe
   if(nextImg.length != 0) {
-  nextImg.addClass('current-img');
+  nextImg.addClass('current');
+  nextDot.removeClass('far');
+  nextDot.addClass('fas current');
   } else {
   nextImg = $('img:first-child');
-  nextImg.addClass('current-img');
+  nextImg.addClass('current');
+  nextDot = $('.bullets-box i:first-child');
+  nextDot.removeClass('far')
+  nextDot.addClass('fas current');
 }
-})
+}, 3000)
 
-$('.prev-box').click(function() {
-  var currentImg = $('img.current-img');
-
+/*$('.prev-box').click(function() {
+  var currentImg = $('img.current');
+  var currentDot = $('.bullets-box i.current')
   //rimuovo la classe
-  currentImg.removeClass('current-img');
-
+  currentImg.removeClass('current');
+  currentDot.removeClass('fas current');
+  currentDot.addClass('far');
   var previousImg = currentImg.prev();
-  //scalo di uno e aggiungo la classe
+  var previousDot = currentDot.prev();
 
+  //scalo di uno e aggiungo la classe
   if(previousImg.length != 0) {
-  previousImg.addClass('current-img');
+  previousImg.addClass('current');
+  previousDot.removeClass('far');
+  previousDot.addClass('fas current');
   } else {
   previousImg = $('img:last-child');
-  previousImg.addClass('current-img');
+  previousImg.addClass('current');
+  previousDot = $('.bullets-box i:last-child');
+  previousDot.removeClass('far')
+  previousDot.addClass('fas current');
 }
-})
+})*/
